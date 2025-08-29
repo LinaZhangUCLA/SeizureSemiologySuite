4c46bd6d51486766c6f88d3714044ca96831e679
AmBerr11 <108852902+AmBerr11@users.noreply.github.com>
2025-08-29 12:34:44 -0700

    Update run_internvl3.5_inf.sh

--- inference/run_internvl3.5_inf.sh
+++ inference/run_internvl3.5_inf.sh
@@ -1,3 +1,7 @@
+conda activate internvl3_5
+
+huggingface-cli login  # When prompted, paste your token (e.g., hf_xxx...) and press Enter to confirm.
+
 python /mnt/SSD3/xinyi/benchmark/task1_internvl3_5_8B.py \
   --gpu 1 \
   --tp 1 \
@@ -10,3 +14,6 @@ python /mnt/SSD3/xinyi/benchmark/task1_internvl3_5_8B.py \
   --dataset_dir /mnt/SSD3/tengyou/seizure_videos/segments/all_dataset/ \
   --output_dir  /mnt/SSD3/xinyi/benchmark/ \
   --cache_dir   /mnt/SSD3/xinyi/benchmark/model_cache
+
+  
+

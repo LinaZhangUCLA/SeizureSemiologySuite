0710fb6a959cef4401f94c228d4215fb95abba7c
Lina Zhang <134445739+CedrusLNZ@users.noreply.github.com>
2025-08-29 12:37:41 -0700

    Update run_qwen_inf2.sh

--- inference/run_qwen_inf2.sh
+++ inference/run_qwen_inf2.sh
@@ -41,6 +41,6 @@ python /home/hubing/SeizureSemiologyBench/inference/task1_Qwen-2.5-VL-7B-Instruc
     --output_dir /home/hubing/SeizureSemiologyBench/output2 \
     --model_name Qwen/Qwen2.5-VL-7B-Instruct \
     --dataset_dir /home/hubing/ucla/second_half_videos \
-    --cache_dir /home/hubing/SeizureSemiologyBench/cache 
+    --cache_dir /home/hubing/SeizureSemiologyBench/cache \ 
 
 echo "Done!"

1291eb7b59a0b0dd1dde4e91c90ef78cd0f3d70b
Tengyou Xu <tengyoux@ucla.edu>
2025-08-29 12:20:25 -0700

    update task1

--- inference/run_qwen_inf1.sh
+++ inference/run_qwen_inf1.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-# Simple script to run Qwen inference
-# Usage: ./run_qwen_inf.sh [gpu_id] [num_videos]
+# This is a simple example script to show how to set up the input parameters 
+# for the inference script for SeizureSemiologyBench Task 1
 
-# Set GPU (default: 3)
-GPU=${1:-7}
+# python /home/hubing/SeizureSemiologyBench/inference/task1_Qwen-2.5-VL-7B-Instruct.py \
+# --dataset_dir /home/hubing/ucla/first_half_videos \
 
-# Set number of videos (default: 10)
-NUM_VIDEOS=${2:-10}
-
-echo "Running Qwen inference on GPU $GPU with $NUM_VIDEOS videos..."
-
-# Activate virtual environment
-eval "$(conda shell.bash hook)"
-conda activate qwenvl
-
-# Check if activation was successful
-if [ $? -ne 0 ]; then
-  echo "Failed to activate conda environment qwenvl. Exiting."
-  exit 1
-fi
-
-echo "Conda environment qwenvl activated."
-
-
-# model_name options
-# Qwen/Qwen2.5-VL-7B-Instruct   1GPU  
-# Qwen/Qwen2.5-VL-32B-Instruct  2GPU
-# Qwen/Qwen2.5-VL-72B-Instruct  4GPU
-
-
-# Run the inference script
-python /home/hubing/SeizureSemiologyBench/inference/task1_Qwen-2.5-VL-7B-Instruct.py \
-    --gpu 0 \
-    --max_videos -1 \
-    --max_frames 60 \
-    --fps 2 \
-    --max_new_tokens 2048 \
-    --max_retries 10 \
-    --output_dir /home/hubing/SeizureSemiologyBench/output \
+python /mnt/SSD3/tengyou/SeizureSemiologyBench/inference/task1_Qwen-2.5-VL-7B-Instruct.py \
+    --gpu 2,3 \
     --model_name Qwen/Qwen2.5-VL-7B-Instruct \
-    --dataset_dir /home/hubing/ucla/first_half_videos \
-    --cache_dir /home/hubing/SeizureSemiologyBench/cache    
+    --dataset_dir /mnt/SSD3/tengyou/seizure_videos/segments/all_dataset \
+    --cache_dir /mnt/SSD3/tengyou/model_cache \
+    --output_dir /mnt/SSD3/tengyou/inference \
+    --videos_range 0,1 \
+    --disable_logs False \
 
 echo "Done!"
+
+# ======================================= Notice =======================================
+# 1. If no gpu is specified, the script will use cuda:0
+# 2. If no model name is specified, the script will use Qwen/Qwen2.5-VL-7B-Instruct
+# 3. No default dataset directory is specified. You need to specify the dataset directory.
+# 4. If no output directory is specified, the script will create a "output" folder at the current directory.
+# 5. If no cache directory is specified, the script will create a "model_cache" folder at the current directory.
+# 6. If no videos range is specified, the script will run the first 1 videos.
+# 7. If no disable_logs is specified, the script will print the logs.
+
+# You can directly run the python script after setting the parameters as flags.
+# You can also change the parameters in the script and run it.
+# ======================================= END =======================================

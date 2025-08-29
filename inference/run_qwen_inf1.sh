fb8f3ac07201777192f690e907f05544b2721fd7
LinaZhangUSC <zhanglinaee@gmail.com>
2025-08-29 00:23:01 -0700

    add filee

---
+++ inference/run_qwen_inf1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Simple script to run Qwen inference
+# Usage: ./run_qwen_inf.sh [gpu_id] [num_videos]
+
+# Set GPU (default: 3)
+GPU=${0}
+
+# Set number of videos (default: 10)
+NUM_VIDEOS=${-1}
+
+echo "Running Qwen inference on GPU $GPU with $NUM_VIDEOS videos..."
+
+# Activate virtual environment
+eval "$(conda shell.bash hook)"
+conda activate qwenvl
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to activate conda environment qwenvl. Exiting."
+  exit 1
+fi
+
+echo "Conda environment qwenvl activated."
+
+
+# model_name options
+# Qwen/Qwen2.5-VL-7B-Instruct   1GPU  
+# Qwen/Qwen2.5-VL-32B-Instruct  2GPU
+# Qwen/Qwen2.5-VL-72B-Instruct  4GPU
+
+
+# Run the inference script
+python /home/hubing/SeizureSemiologyBench/inference/task1_Qwen-2.5-VL-7B-Instruct.py \
+    --gpu 0 \
+    --max_videos -1 \
+    --max_frames 60 \
+    --fps 2 \
+    --max_new_tokens 2048 \
+    --max_retries 10 \
+    --output_dir /home/hubing/SeizureSemiologyBench/output\
+    --model_name Qwen/Qwen2.5-VL-7B-Instruct \
+    --dataset_dir /home/hubing/ucla/first_half_videos \
+    --cache_dir /home/hubing/SeizureSemiologyBench/cache \
+
+echo "Done!"

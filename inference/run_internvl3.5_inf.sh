9fb8944b57a13f50dce2e690d0673007cd5f8b0e
AmBerr11 <108852902+AmBerr11@users.noreply.github.com>
2025-08-29 12:03:41 -0700

    Update run_internvl3.5_inf.sh

---
+++ inference/run_internvl3.5_inf.sh
@@ -0,0 +1,12 @@
+python /mnt/SSD3/xinyi/benchmark/task1_internvl3_5_8B.py \
+  --gpu 1 \
+  --tp 1 \
+  --max_frames 60 \
+  --max_videos -1 \
+  --disable_logs True \
+  --max_retries 10 \
+  --max_new_tokens 2048 \
+  --model_name OpenGVLab/InternVL3_5-8B \
+  --dataset_dir /mnt/SSD3/tengyou/seizure_videos/segments/all_dataset/ \
+  --output_dir  /mnt/SSD3/xinyi/benchmark/ \
+  --cache_dir   /mnt/SSD3/xinyi/benchmark/model_cache

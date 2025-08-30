96a135ec4f7c3a64677f9a0fa2a8fe6140df17ea
LinaZhangUSC <zhanglinaee@gmail.com>
2025-08-30 10:58:30 -0700

    dev

--- inference/task1_internvl35_38B/run_task1_internvl35-38B3.sh
+++ inference/task1_internvl35_38B/run_task1_internvl35-38B3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
+
 # Activate virtual environment
 eval "$(conda shell.bash hook)"
-conda activate qwenvl
-
+# conda activate qwenvl
+conda activate internvl3_5
 
 # Check if activation was successful
 if [ $? -ne 0 ]; then
@@ -12,22 +13,24 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "Conda environment qwenvl activated."
+echo "Conda environment internvl3_5 activated."
 
 
 # model_name options
 # Qwen/Qwen2.5-VL-7B-Instruct   1GPU  
 # Qwen/Qwen2.5-VL-32B-Instruct  2GPU
 # Qwen/Qwen2.5-VL-72B-Instruct  4GPU
+
 # video_range 1-2314  eg.1-1000, 1001-2000, 2001-2314
 
 # Run the inference script
-python task1_Qwen-2.5-VL-7B-Instruct.py \
-    --gpu 4,5,6,7 \
-    --videos_range 1-1000 \
+python task1_internvl3.5_8B.py \
+    --gpu 4,5 \
+    --tp 2 \
+    --videos_range 1201-1800 \
     --output_dir /home/hubing/SeizureSemiologyBench/output \
-    --model_name Qwen/Qwen2.5-VL-72B-Instruct \
-    --dataset_dir /home/hubing/ucla/all_vidoes \
+    --model_name OpenGVLab/InternVL3_5-38B \
+    --dataset_dir /home/hubing/SeizureSemiologyBench/ucla  \
     --cache_dir /home/hubing/SeizureSemiologyBench/cache 
 
 echo "Done!"

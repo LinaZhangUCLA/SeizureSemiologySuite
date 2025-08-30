60dc3c80b7a3b46fbc361f4a756cabf2cb5f14bd
LinaZhangUSC <zhanglinaee@gmail.com>
2025-08-29 23:01:28 -0700

    dev

--- inference/run_task1_Qwen25-VL-32B4.sh
+++ inference/run_task1_Qwen25-VL-32B4.sh
@@ -22,8 +22,8 @@ echo "Conda environment qwenvl activated."
 # video_range 1-2314  eg.1-1000, 1001-2000, 2001-2314
 # Run the inference script
 python task1_Qwen-2.5-VL-7B-Instruct.py \
-    --gpu 2,3 \
-    --videos_range 1-1000 \
+    --gpu 6,7 \
+    --videos_range 1801-2341 \
     --output_dir /home/hubing/SeizureSemiologyBench/output \
     --model_name Qwen/Qwen2.5-VL-32B-Instruct \
     --dataset_dir /home/hubing/SeizureSemiologyBench/ucla \

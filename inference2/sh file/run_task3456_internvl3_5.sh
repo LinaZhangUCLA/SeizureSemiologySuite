265cef8c8f95628e43ee62c21465026f4e665658
jerry66s6 <148895489+jerry66s6@users.noreply.github.com>
2025-09-17 16:37:10 -0700

    Update run_task3456_internvl3_5.sh

--- inference2/sh file/run_task3456_internvl3_5.sh
+++ inference2/sh file/run_task3456_internvl3_5.sh
@@ -17,7 +17,7 @@ echo "Conda environment internvl3 activated."
 python /home/lina/ssb/SeizureSemiologyBench/inference/task3456_internvl3_5test.py \
   --gpu 0 \
   --model_name OpenGVLab/InternVL3_5-8B \
-  --dataset_dir /mnt/SSD3/lina/ucla2 \
+  --dataset_dir /mnt/SSD3/lost_videos/InternVL3_5-8B \
   --cache_dir /mnt/SSD3/xinyi/benchmark/model_cache \
   --output_dir /home/lina/SeizureSemiologyBench\inten1147 \
   --videos_range 1-2413 \

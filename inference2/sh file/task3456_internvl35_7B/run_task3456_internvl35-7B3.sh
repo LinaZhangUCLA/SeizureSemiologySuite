e1286a8fdb3a6eaa0f269ed5f7dc940a7481b244
jerry66s6 <148895489+jerry66s6@users.noreply.github.com>
2025-09-17 16:36:12 -0700

    Update run_task3456_internvl35-7B3.sh

--- inference2/sh file/task3456_internvl35_7B/run_task3456_internvl35-7B3.sh
+++ inference2/sh file/task3456_internvl35_7B/run_task3456_internvl35-7B3.sh
@@ -27,10 +27,10 @@ echo "Conda environment internvl3_5 activated."
 python ../task3456_internvl3_5.py \
     --gpu 2 \
     --tp 1 \
-    --videos_range 601-900 \
+    --videos_range 601-788 \
     --output_dir /home/hubing/SeizureSemiologyBench/output \
     --model_name OpenGVLab/InternVL3_5-8B \
-    --dataset_dir /home/hubing/SeizureSemiologyBench/ucla2  \
+    --dataset_dir /home/hubing/SeizureSemiologyBench/lost_videos/InternVL3_5-8B  \
     --cache_dir /home/hubing/SeizureSemiologyBench/cache 
 
 echo "Done!"

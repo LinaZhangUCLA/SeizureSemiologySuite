5e74483bf4f7b7deac5a4389bf46529e361160cd
Prateik Sinha <emailprateik@gmail.com>
2025-10-24 01:22:49 -0700

    task12 qwen3vl multiple scripts

---
+++ inference3/task12_qwen3vl_32b.sh/run_task12_qwen3vl_32b_2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+# Activate virtual environment
+# eval "$(conda shell.bash hook)"
+# conda activate qwenvl
+# conda activate qwen3vl_moe
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /mnt/SSD3/lina/my_conda_env/qwen3vl_moe
+which python
+python -c 'import sys; print(sys.executable)'
+
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to activate conda environment qwenvl. Exiting."
+  exit 1
+fi
+
+echo "Conda environment qwen3vl_moe activated."
+
+
+# model_name options
+# Qwen/Qwen2.5-VL-7B-Instruct   1GPU  
+# Qwen/Qwen2.5-VL-32B-Instruct  2GPU
+# Qwen/Qwen2.5-VL-72B-Instruct  4GPU
+
+# video_range 1-2314  eg.1-1000, 1001-2000, 2001-2314
+
+# Run the inference script
+python task12_Qwen3-VL-32B-Instruct.py \
+    --gpu 2,3 \
+    --videos_range 601-1200 \
+    --output_dir /mnt/SSD1/prateik/SeizureSemiologyBench/output \
+    --model_name Qwen/Qwen3-VL-32B-Instruct \
+    --dataset_dir /mnt/SSD3/lina/ucla2/ssbench/task1256_segment_60s \
+    --cache_dir /mnt/SSD4/prateik/cache 
+
+echo "Done!"
